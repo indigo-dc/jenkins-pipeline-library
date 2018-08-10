f49d82f303ea72f7a2207a8a0d3eed595efb2ca7
Pablo Orviz <orviz@ifca.unican.es>
2018-08-10 11:54:55 +0200

    Parameterize

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/groovy
-def call() {
-    if (env.BRANCH_NAME == 'master') {
-        IMAGE_ID = env.docker_repo + ':latest'
+def call(repository, scm_branch) {
+    if (scm_branch == 'master') {
+        IMAGE_ID = repository + ':latest'
     }
     else {
-        IMAGE_ID = env.docker_repo + ':' + env.TAG_NAME
+        IMAGE_ID = repository + ':' + scm_branch
     }
     // OJO AQUI, hardcoded
-    IMAGE_ID = env.docker_repo + ':latest'
+    IMAGE_ID = repository + ':latest'
     
     sh "docker build --force-rm -t $IMAGE_ID ."
     return IMAGE_ID
