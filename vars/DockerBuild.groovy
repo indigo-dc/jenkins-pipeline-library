be10eb42d1c58916d8061526d9115e02b09ac0bd
Pablo Orviz <orviz@ifca.unican.es>
2018-08-10 11:34:47 +0200

    Remove script

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,15 +1,14 @@
 #!/usr/bin/groovy
 def call() {
-    script {
-        if (env.BRANCH_NAME == 'master') {
-            IMAGE_ID = env.docker_repo + ':latest'
-        }
-        else {
-            IMAGE_ID = env.docker_repo + ':' + env.TAG_NAME
-        }
-        // OJO AQUI, hardcoded
+    if (env.BRANCH_NAME == 'master') {
         IMAGE_ID = env.docker_repo + ':latest'
     }
+    else {
+        IMAGE_ID = env.docker_repo + ':' + env.TAG_NAME
+    }
+    // OJO AQUI, hardcoded
+    IMAGE_ID = env.docker_repo + ':latest'
+    
     sh "docker build --force-rm -t $IMAGE_ID ."
     return $IMAGE_ID
 }
