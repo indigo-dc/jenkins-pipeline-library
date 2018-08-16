bb992622bf1f4ab4343c764a020d24f83f1e31cf
Pablo Orviz <orviz@ifca.unican.es>
2018-08-16 13:28:31 +0200

    Add argument to point to different tox file

--- vars/ToxEnvRun.groovy
+++ vars/ToxEnvRun.groovy
@@ -1,4 +1,10 @@
 #!/usr/bin/groovy
-def call(testenv) {
-    sh(script: 'tox -e '+testenv)
+
+def call(testenv, filename=null) {
+    opts = ['-e '+testenv]
+    if (filename) {
+        opts += '-c '+filename
+    }
+    cmd = ['tox'] + opts
+    sh(script: cmd.join(' '))
 }
