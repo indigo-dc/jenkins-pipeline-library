5c94a7e0276bd2c949326073084563477a3de978
Pablo Orviz <orviz@ifca.unican.es>
2018-08-16 14:32:07 +0200

    Add support for list of packages

--- vars/PipRequirements.groovy
+++ vars/PipRequirements.groovy
@@ -1,10 +1,20 @@
 #!/usr/bin/groovy
-def call(content, filename) {
+
+call("guachuguei", "")
+
+def call(packages, filename) {
+    if (packages instanceof List) {
+        package_str = packages.join('\n')
+    }
+    else { // assume String
+        package_str = packages
+    }
+
     if (fileExists(filename)) {
         def readContent = readFile filename
-        writeFile file: filename, text: readContent+'\n'+content
+        writeFile file: filename, text: readContent+'\n'+package_str
     }
     else {
-  	    writeFile file: filename, text: content
+  	    writeFile file: filename, text: package_str
     }
 }
