938d3653213d2923e55041e6a32701d82b014aad
Pablo Orviz <orviz@ifca.unican.es>
2018-09-25 10:29:12 +0200

    Fix parameterization

--- vars/JiraIssueCreate.groovy
+++ vars/JiraIssueCreate.groovy
@@ -1,17 +1,14 @@
 #!/usr/bin/groovy
 
-def call(site_id, project_id, summary, description, label, issue_type='Task') {
+def call(site_id, project_id, summary, description, label, issue_type) {
 	def testIssue = [fields: [ 
-		// id or key must present for project.
-		project: [id: project_id],
-		summary: summary,
-		description: description,
-		//customfield_1000: 'customValue',
-		// id or name must present for issueType.
-		issuetype: [name: issue_type],
-		labels: [label]]]
-
-	response = jiraNewIssue issue: testIssue, site: site_id
+		project: [id: "${project_id}"],
+		summary: "${summary}",
+		description: "${description}",
+		issuetype: [name: "${issue_type}"],
+		labels: ["${label}"]]]
+    
+    response = jiraNewIssue issue: testIssue, site: "${site_id}"
 
 	echo response.successful.toString()
 	echo response.data.toString()
