a8d7b6305d5a2ceacf9a4fb81c107d24ffe0d4cb
Pablo Orviz <orviz@ifca.unican.es>
2018-10-25 11:40:35 +0200

    Fix variable interpolation

--- vars/OWASPDependencyCheckRun.groovy
+++ vars/OWASPDependencyCheckRun.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/groovy
 
 def call(src_path, project="My Project") {
-	OWASP_DIR=[env.WORKSPACE, 'OWASP'].join('/')
-	OWASP_DATA_DIR=[env.WORKSPACE, 'OWASP', 'data'].join('/')
-	OWASP_REPORT_DIR=[env.WORKSPACE, 'OWASP', 'report'].join('/')
+	def OWASP_DIR=[env.WORKSPACE, 'OWASP'].join('/')
+	def OWASP_DATA_DIR=[env.WORKSPACE, 'OWASP', 'data'].join('/')
+	def OWASP_REPORT_DIR=[env.WORKSPACE, 'OWASP', 'report'].join('/')
 	
 	sh "mkdir -p $OWASP_DATA_DIR"
 	sh "mkdir -p $OWASP_REPORT_DIR"
@@ -11,8 +11,10 @@ def call(src_path, project="My Project") {
 	
 	dir("$src_path") {
 		timeout(time: 10, unit: "MINUTES") {
-			withDockerContainer(image: 'owasp/dependency-check', args: "--entrypoint '' --volume $src_path:/src --volume $OWASP_DATA_DIR:/usr/share/dependency-check/data --volume $OWASP_REPORT_DIR:/report") {
-				sh "/usr/share/dependency-check/bin/dependency-check.sh --project 'OWASP Dependency Check for $project' --scan /src --format 'ALL' --enableExperimental"
+			withDockerContainer(
+                image: 'owasp/dependency-check',
+                args: "--entrypoint '' --volume ${src_path}:/src --volume ${OWASP_DATA_DIR}:/usr/share/dependency-check/data --volume ${OWASP_REPORT_DIR}:/report") {
+				    sh "/usr/share/dependency-check/bin/dependency-check.sh --project 'OWASP Dependency Check for $project' --scan /src --format 'ALL' --enableExperimental"
 			}
 		}
 	}
