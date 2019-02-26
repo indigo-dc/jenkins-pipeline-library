a7c8557b8cd6e3faf0c98100837e6cbe74bc98d4
Pablo Orviz <orviz@ifca.unican.es>
2019-02-26 10:40:14 +0100

    Allow multiple tags for the same repo

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -4,26 +4,32 @@
  * Builds a Docker image.
  *
  * @param repository Docker registry's repository name [mandatory]
- * @param tag Tag for the Docker image (aligned with SCM branch) [named, optional]
- * @param build_args Build args for Docker image creation [named, optional]
- * @param build_dir Dockerfile location [named, optional]
+ * @param tag Tag for the Docker image (aligned with SCM branch) [List, String] [named, optional]
+ * @param build_args Build args for Docker image creation [List] [named, optional]
+ * @param build_dir Dockerfile location [String] [named, optional]
  * @return Docker image ID
  */
 def call(Map docker=[:], String repository) {
-    if (docker.tag in ['master', 'latest', 'null', null]) {
-        id = repository + ':latest'
+    ids = [] 
+    if (docker.tag instanceof List) {
+        docker.tag.each {
+            ids.push((repository + ':' + it).toLowerCase())
+        }
+    }
+    else if (docker.tag in ['master', 'latest', 'null', null]) {
+        ids.push((repository + ':latest').toLowerCase())
     }
     else {
-        id = repository + ':' + docker.tag
+        ids.push((repository + ':' + docker.tag).toLowerCase())
     }
-    id = id.toLowerCase()
-    
+    id_str = ids.join(' -t ')
+
     build_args = ''
     docker.build_args.each {
         build_args += "--build-arg ${it} "
     }
 
-    cmd = "docker build --no-cache --force-rm -t $id $build_args"
+    cmd = "docker build --no-cache --force-rm -t $id_str $build_args"
     cmd = cmd.trim()
 
     if (docker.build_dir == null) {
