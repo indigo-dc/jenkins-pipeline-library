945bdb76b57c4bd18d96dd3fbd3b2c3f27e5f447
Pablo Orviz <orviz@ifca.unican.es>
2019-03-01 09:19:04 +0100

    Fix: DockerBuild returns a List that DockerPush receives as input

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -3,12 +3,14 @@
 /**
  * Pushes Docker images to Docker Hub registry.
  *
- * @param image Image ID [mandatory]
+ * @param image_ids List of Docker image IDs [mandatory]
  * @param url Docker registry URL [default]
  * @see https://plugins.jenkins.io/docker-workflow
  */
-def call(String image, String url='') {
+def call(List image_ids, String url='') {
     withDockerRegistry([credentialsId: 'indigobot', url: url]) {
-        sh "docker push $image"
+        image_ids.each {
+            sh "docker push ${it}"
+        }
     }
 }
