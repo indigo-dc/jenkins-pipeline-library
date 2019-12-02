9df270c9d3b9ed0e1f61e2fb0fb18c14cfd5050e
Pablo Orviz <orviz@ifca.unican.es>
2019-12-02 17:02:10 +0100

    Add parameter capability to build Jenkins jobs

--- vars/JenkinsBuildJob.groovy
+++ vars/JenkinsBuildJob.groovy
@@ -6,8 +6,9 @@
  * @param location Jenkins' location of the job [mandatory]
  * @see https://plugins.jenkins.io/pipeline-build-step
  */
-def call(String location) {
+def call(String location, List parameters=[]) {
     build job: "${location}", 
+    parameters: parameters, 
     propagate: true, 
     wait: true
 }
