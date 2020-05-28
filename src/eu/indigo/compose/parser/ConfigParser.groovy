b26998a674edecd68ba5f5c8356277260b477b39
Pablo Orviz <orviz@ifca.unican.es>
2020-05-28 17:06:00 +0200

    Remove old ProjectConfiguration attributes/methods.

--- src/eu/indigo/compose/parser/ConfigParser.groovy
+++ src/eu/indigo/compose/parser/ConfigParser.groovy
@@ -21,24 +21,9 @@ class ConfigParser implements Serializable {
 
         projectConfiguration.buildNumber = env.BUILD_ID
 
-        // parse the environment variables
-        projectConfiguration.environment    = parseEnvironment(yaml.environment)
+        projectConfiguration.environment = parseEnvironment(yaml.environment)
 
-        // parse the execution steps
-        projectConfiguration.steps          = parseSteps(yaml.steps)
-
-        // parse the necessary services
-        projectConfiguration.services   = parseServices(yaml.services)
-
-        // load the dockefile
-        projectConfiguration.dockerfile = parseDockerfile(yaml.config)
-
-        // load the project name
         projectConfiguration.projectName = parseProjectName(yaml.config)
-
-        projectConfiguration.env = env
-
-        projectConfiguration.dockerConfiguration = new DockerConfiguration(projectConfiguration: projectConfiguration)
 
         projectConfiguration.timeout = yaml.timeout ?: DEFAULT_TIMEOUT
 
@@ -129,70 +114,6 @@ class ConfigParser implements Serializable {
         }
 
         return environment.collect { k, v -> "${k}=${v}"}
-    }
-
-    static def parseSteps(def yamlSteps) {
-        List<Step> steps = yamlSteps.collect { k, v ->
-            Step step = new Step(name: k)
-
-            // a step can have one or more commands to execute
-            v.each {
-                step.commands.add(it)
-            }
-            return step
-        }
-        return new Steps(steps: steps)
-    }
-
-    static def parseServices(def steps) {
-        def services = []
-
-        steps.each {
-            def service = it.tokenize(':')
-            def version = service.size() == 2 ? service.get(1) : LATEST
-            def instance = getServiceClass(service.get(0).capitalize())?.newInstance()
-
-            services.add([service: instance, version: version])
-        }
-
-        services.add([service: new Base(), version: LATEST])
-
-        return services
-    }
-
-    static def getServiceClass(def name) {
-        // TODO: Refactor this
-        switch(name) {
-            case "Postgres":
-                return Postgres
-                break
-            case "Postgis":
-                return Postgis
-                break
-            case "Redis":
-                return Redis
-                break
-            case "Mssql":
-                return Mssql
-                break
-            case "Mysql":
-                return Mysql
-                break
-            case "Mongodb":
-                return Mongodb
-                break
-            case "Elasticsearch":
-                return Elasticsearch
-                break
-        }
-    }
-
-    static def parseDockerfile(def config) {
-        if (!config || !config["dockerfile"]) {
-            return "Dockerfile"
-        }
-
-        return config["dockerfile"]
     }
 
     static def parseProjectName(def config) {
