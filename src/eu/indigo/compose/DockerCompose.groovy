682630854e425d139312b03631fde9ef43d3ad96
Pablo Orviz <orviz@ifca.unican.es>
2020-06-09 13:06:20 +0200

    test

--- src/eu/indigo/compose/DockerCompose.groovy
+++ src/eu/indigo/compose/DockerCompose.groovy
@@ -135,11 +135,11 @@ class DockerCompose extends JenkinsDefinitions implements Serializable {
             // Checkout repositories to workspace with defined repository name
             projectConfig.config.project_repos.each { repo_name, repo_confs ->
                 steps.checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: repo_confs.repo]], \
-                               branches: [[name: repo_confs.branch]], \
-                               extensions: [[$class: 'CleanCheckout', deleteUntrackedNestedRepositories: true], \
-                                            [$class: 'GitLFSPull'], \
-                                            [$class: 'RelativeTargetDirectory', relativeTargetDir: repo_name], \
-                                            [$class: 'ScmName', name: repo_name]] ], \
+                               branches: [[name: repo_confs.branch]] ], \
+                               //extensions: [[$class: 'CleanCheckout', deleteUntrackedNestedRepositories: true], \
+                               //             [$class: 'GitLFSPull'], \
+                               //             [$class: 'RelativeTargetDirectory', relativeTargetDir: repo_name], \
+                               //             [$class: 'ScmName', name: repo_name]] ], \
                                changelog: false, poll: false
             }
 
