32111ec991f286d53c109022d1c868fb1e1045f8
Pablo Orviz <orviz@ifca.unican.es>
2020-06-11 16:42:12 +0200

    User pseudo-tty

--- src/eu/indigo/compose/DockerCompose.groovy
+++ src/eu/indigo/compose/DockerCompose.groovy
@@ -121,9 +121,11 @@ class DockerCompose extends JenkinsDefinitions implements Serializable {
     * @see https://docs.docker.com/compose/reference/exec/
     */
     def composeToxRun(Map args, String service, String testenv, Tox tox) {
-        String cmd = parseParam(_f, args.composeFile) + ' ' + parseParam(_w, args.workdir) + ' exec ' + \
+        String cmd2 = parseParam(_f, args.composeFile) + ' ' + parseParam(_w, args.workdir) + ' exec -T ' + \
+                " $service " + 'ls -l /deepaas-testing'
+        steps.sh "docker-compose $cmd2"
+        String cmd = parseParam(_f, args.composeFile) + ' ' + parseParam(_w, args.workdir) + ' exec -T ' + \
                 " $service " + tox.runEnv(testenv, toxFile: args.toxFile)
-
         steps.sh "docker-compose $cmd"
     }
 
