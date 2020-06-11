84f365815709a64b680d651408c68917f6a4f991
Pablo Orviz <orviz@ifca.unican.es>
2020-06-11 08:28:04 +0200

    test

--- src/eu/indigo/compose/DockerCompose.groovy
+++ src/eu/indigo/compose/DockerCompose.groovy
@@ -55,7 +55,6 @@ class DockerCompose extends JenkinsDefinitions implements Serializable {
     def composeExec(Map args, String service, String command) {
         String cmd = parseParam(_f, args.composeFile) + ' exec ' + \
                 parseParam(_w, args.workdir) + " $service $command"
-
         steps.sh "docker-compose $cmd"
     }
 
@@ -69,7 +68,6 @@ class DockerCompose extends JenkinsDefinitions implements Serializable {
     */
     def composeUp(Map args, String serviceIds='') {
         String cmd = parseParam(_f, args.composeFile) + " up $serviceIds"
-
         steps.sh "docker-compose $cmd --detach"
     }
 
@@ -121,7 +119,7 @@ class DockerCompose extends JenkinsDefinitions implements Serializable {
     */
     def composeToxRun(Map args, String service, String testenv, Tox tox) {
         String cmd = parseParam(_f, args.composeFile) + ' exec ' + \
-                parseParam(_w, args.workdir) + \
+                //parseParam(_w, args.workdir) + \
                 " $service " + tox.runEnv(testenv, toxFile: args.toxFile)
 
         steps.sh "docker-compose $cmd"
