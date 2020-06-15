f9f97477b8d1bef2e8726f9aaa6a9e4be59e0c8d
Samuel Bernardo <samuel@lip.pt>
2020-06-15 13:51:44 +0100

    Change previous merge implementation to a different approach using flatmap approach to merge all values and get rid of recursive calls
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- src/eu/indigo/compose/parser/ConfigParser.groovy
+++ src/eu/indigo/compose/parser/ConfigParser.groovy
@@ -50,7 +50,7 @@ class ConfigParser extends JenkinsDefinitions implements Serializable {
 
         (sources[0].entrySet() + sources[1].entrySet()).each { entry ->
             steps.echo "result = $result\nkey = ${entry.key}\nvalue = ${entry.value}"
-            result[entry.key] = result.containsKey(entry.key) && result[entry.key].getClass == Map ?
+            result[entry.key] = result.containsKey(entry.key) && result[entry.key].getClass() == Map ?
                 [:] << result[entry.key] << entry.value
                 : entry.value
         }
