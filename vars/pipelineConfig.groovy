c1135bb634759c89236885f38bcb9fe87018c980
Samuel Bernardo <samuel@lip.pt>
2020-06-19 23:58:54 +0100

    Correct config.yml syntax to use withCredentials. Use different validator image with jpl-validator 1.1.0 from worsica.
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- vars/pipelineConfig.groovy
+++ vars/pipelineConfig.groovy
@@ -43,7 +43,7 @@ def call(
 }
 
 def validate(String configFile) {
-    def validatorDockerImage = 'eoscsynergy/jpl-validator:1.0.0'
+    def validatorDockerImage = 'worsica/jpl-validator:1.1.0'
     def cmd = 'docker pull ' + "$validatorDockerImage &&" +
               'docker run --rm -v "$PWD:/sqa" ' + "$validatorDockerImage /sqa/${configFile}"
     return sh(returnStatus: true, script: cmd)
