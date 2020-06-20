98a1ab49ff78487c7c5ea6ebef07f74f8ddb93f8
Samuel Bernardo <samuel@lip.pt>
2020-06-20 18:14:42 +0100

    Correct closure call with closure argument
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- vars/buildStages.groovy
+++ vars/buildStages.groovy
@@ -1,28 +1,27 @@
 import eu.indigo.compose.ProjectConfiguration
 import eu.indigo.compose.ComposeFactory
 
-def timeoutClosure = { Closure block ->
-    if (projectConfig.timeout) {
-        timeout(time: projectConfig.timeout, activity: true, unit: 'SECONDS') {
-            block()
-        }
-    } else {
-        block()
-    }
-}
-
-def environmentClosure = { Closure block ->
-    if (projectConfig.environment) {
-        withEnv(projectConfig.nodeAgent.envToStep(projectConfig.environment)) {
-            block()
-        }
-    } else {
-        block()
-    }
-}
-
 def call(ProjectConfiguration projectConfig) {
     try {
+        def timeoutClosure = { Closure block ->
+            if (projectConfig.timeout) {
+                timeout(time: projectConfig.timeout, activity: true, unit: 'SECONDS') {
+                    block()
+                }
+            } else {
+                block()
+            }
+        }
+        def environmentClosure = { Closure block ->
+            if (projectConfig.environment) {
+                withEnv(projectConfig.nodeAgent.envToStep(projectConfig.environment)) {
+                    block()
+                }
+            } else {
+                block()
+            }
+        }
+
         timeoutClosure {
             environmentClosure {
                 projectConfig.nodeAgent.processStages(projectConfig)
