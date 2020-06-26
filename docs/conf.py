f3b48c37c31ab6689ba8a69b14cdc18e1215727d
Pablo Orviz <orviz@ifca.unican.es>
2020-06-26 10:05:52 +0200

    Filter out versions that are neither stable/ or release/

--- docs/conf.py
+++ docs/conf.py
@@ -341,3 +341,6 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+# -- Options for sphinx-versioning -------------------------------------------
+scv_whitelist_branches = ('(stable|release){1}/.+',)
