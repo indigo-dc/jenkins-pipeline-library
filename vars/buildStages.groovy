390756bdc897eb66caf578bdc551d65b0ee9f52e
Samuel Bernardo <samuel@lip.pt>
2020-07-13 18:35:35 +0100

    Correct reject access exception in closures in buildStages, since they are not allowed in Jenkins sandbox. Transform them into methods so they can be CPS transformed by Jenkins.
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- vars/buildStages.groovy
+++ vars/buildStages.groovy
@@ -1,28 +1,30 @@
 import eu.indigo.compose.ProjectConfiguration
 import eu.indigo.compose.ComposeFactory
 
+def timeoutClosure(ProjectConfiguration projectConfig, Closure block) {
+    if (projectConfig.timeout) {
+        timeout(time: projectConfig.timeout, activity: true, unit: 'SECONDS') {
+            block()
+        }
+    } else {
+        block()
+    }
+}
+
+def environmentClosure(ProjectConfiguration projectConfig, Closure block) {
+    if (projectConfig.environment) {
+        withEnv(projectConfig.nodeAgent.envToStep(projectConfig.environment)) {
+            block()
+        }
+    } else {
+        block()
+    }
+}
+
+
 def call(ProjectConfiguration projectConfig) {
-    def timeoutClosure = { Closure block ->
-        if (projectConfig.timeout) {
-            timeout(time: projectConfig.timeout, activity: true, unit: 'SECONDS') {
-                block()
-            }
-        } else {
-            block()
-        }
-    }
-    def environmentClosure = { Closure block ->
-        if (projectConfig.environment) {
-            withEnv(projectConfig.nodeAgent.envToStep(projectConfig.environment)) {
-                block()
-            }
-        } else {
-            block()
-        }
-    }
-    
-    timeoutClosure {
-        environmentClosure {
+    timeoutClosure(projectConfig) {
+        environmentClosure(projectConfig) {
             projectConfig.nodeAgent.processStages(projectConfig)
         }
     }
