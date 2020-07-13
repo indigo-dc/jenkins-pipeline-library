678f9f33c1f3d408525f3ecce86936677e517766
Samuel Bernardo <samuel@lip.pt>
2020-07-13 18:21:05 +0100

    Remove try catch block in buildStages since it blocks the backtrace with useful information from Jenkins
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

--- vars/buildStages.groovy
+++ vars/buildStages.groovy
@@ -2,32 +2,28 @@ import eu.indigo.compose.ProjectConfiguration
 import eu.indigo.compose.ComposeFactory
 
 def call(ProjectConfiguration projectConfig) {
-    try {
-        def timeoutClosure = { Closure block ->
-            if (projectConfig.timeout) {
-                timeout(time: projectConfig.timeout, activity: true, unit: 'SECONDS') {
-                    block()
-                }
-            } else {
+    def timeoutClosure = { Closure block ->
+        if (projectConfig.timeout) {
+            timeout(time: projectConfig.timeout, activity: true, unit: 'SECONDS') {
                 block()
             }
+        } else {
+            block()
         }
-        def environmentClosure = { Closure block ->
-            if (projectConfig.environment) {
-                withEnv(projectConfig.nodeAgent.envToStep(projectConfig.environment)) {
-                    block()
-                }
-            } else {
+    }
+    def environmentClosure = { Closure block ->
+        if (projectConfig.environment) {
+            withEnv(projectConfig.nodeAgent.envToStep(projectConfig.environment)) {
                 block()
             }
+        } else {
+            block()
         }
-
-        timeoutClosure {
-            environmentClosure {
-                projectConfig.nodeAgent.processStages(projectConfig)
-            }
+    }
+    
+    timeoutClosure {
+        environmentClosure {
+            projectConfig.nodeAgent.processStages(projectConfig)
         }
-    } catch (Exception e) {
-        error("Exception on buildStages():\n${e}")
     }
 }
