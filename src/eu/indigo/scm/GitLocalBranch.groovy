025201299bc6ca420ce05563eaf056a434d3725f
Samuel Bernardo <samuel@lip.pt>
2020-12-04 12:59:04 +0000

    Revert "Replace exactly with issue code to debug"
    This reverts commit afa955298fcde0061b80b647c0dc70ee1a349dc6.

--- src/eu/indigo/scm/GitLocalBranch.groovy
+++ src/eu/indigo/scm/GitLocalBranch.groovy
@@ -15,32 +15,22 @@ class GitLocalBranch extends Git implements Serializable {
     @Override
     def checkoutRepository() {
         if (_DEBUG_) { steps.echo "** GitLocalBranch.checkoutRepository() **" }
-        steps.checkout transformGitSCM(checkout([
-            branches: scm.branches,
-            doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
-            extensions: scm.extensions + [$class: 'LocalBranch', localBranch: '**'],
-            userRemoteConfigs: [[
-                credentialsId: scm.userRemoteConfigs[0].credentialsId,
-                name: 'origin',
-                refspec: '+refs/heads/*:refs/remotes/origin/*',
-                url: scm.userRemoteConfigs[0].url
-            ]],
-        ]))
+        steps.checkout transformGitSCM([
+                branches: steps.scm.branches,
+                extensions: steps.scm.extensions + [$class: 'LocalBranch', localBranch: '**'],
+                userRemoteConfigs: steps.scm.userRemoteConfigs + [name: 'origin', refspec: '+refs/heads/*:refs/remotes/origin/*']
+            ])
     }
 
     @Override
     def checkoutRepository(String repository, String branch='master', String credentialsId) {
         if (_DEBUG_) { steps.echo "** Git.checkoutRepository($repository, $branch, $credentialsId) **" }
         steps.checkout transformGitSCM([
-                branches: branch,
-                doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
-                extensions: scm.extensions + [$class: 'LocalBranch', localBranch: '**'],
-                userRemoteConfigs: [[
-                    credentialsId: credentialsId,
-                    name: 'origin',
-                    refspec: '+refs/heads/*:refs/remotes/origin/*',
-                    url: repository
-                ]],
+                branches: [[name: "*/${branch}"]],
+                extensions: steps.scm.extensions +
+                            [$class: 'RelativeTargetDirectory', relativeTargetDir: '.'] +
+                            [$class: 'LocalBranch', localBranch: '**'],
+                userRemoteConfigs: steps.scm.userRemoteConfigs + [url: repository, credentialsId: credentialsId, name: 'origin', refspec: '+refs/heads/*:refs/remotes/origin/*']
             ])
     }
 
